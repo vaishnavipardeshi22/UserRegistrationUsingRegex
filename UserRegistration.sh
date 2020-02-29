80229c0d86b9ea11911deffbb8d354e1d29cc204
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-29 12:38:58 +0530

    ADDED : Function to validate first name using regex pattern

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,19 @@
 #!/bin/bash -x
 
 echo " ****************************** WELCOME TO USER REGISTRATION ****************************** "
+
+#FUNCTION FOR VALIDATE FIRST NAME USING REGEX
+function firstName()
+{
+	read -p "Enter first name: " firstName
+	local regexPattern="^[A-Z][a-zA-Z]{2,}$"
+
+	if [[ $firstName =~ $regexPattern ]]
+	then
+		printf "First name is valid."
+	else
+		printf "Invalid first name please enter name starts with capital and has minimum 3 characters."
+	fi
+}
+
+firstName
